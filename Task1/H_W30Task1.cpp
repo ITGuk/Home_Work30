eae8ca07e1e3e9d8458fddba60b6ce55d8f0932e
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-06-09 17:55:45 +0300

    Add files via upload

---
+++ Task1/H_W30Task1.cpp
@@ -0,0 +1,47 @@
+﻿//Task1
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int size = 11;
+	char* arr = new char[size];
+
+	cout << "Enter string: ";
+	cin >> arr;
+
+	bool letters = false;
+	bool digits = false;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] != '\0')
+		{
+			if (arr[i] >= 65 && arr[i] <= 122)
+			{
+				letters = true;
+			}
+			else if (arr[i] >= 48 && arr[i] <= 57)
+			{
+				digits = true;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (letters && !digits)
+	{
+		cout << "\nLetters\n";
+	}
+	else if (!letters && digits)
+	{
+		cout << "\nDigits\n";
+	}
+	else
+	{
+		cout << "\nAnother\n";
+	}
+}
