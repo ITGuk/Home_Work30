61cb474303b46a08953cf78f4adc46f138f02231
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-06-09 18:02:21 +0300

    Add files via upload

---
+++ Task3/H_W30Task3.cpp
@@ -0,0 +1,19 @@
+﻿//Task3
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	double N = 0;
+	cout << "\nEnter: N = "; cin >> N;
+
+	double E = 0;
+	for (double k = N; k < 19; k++)
+	{
+		E += sqrt(pow(sin(k), 2) + pow(cos(N / k), 2));
+		N++;
+	}
+
+	cout << "\nE = " << E;
+}
