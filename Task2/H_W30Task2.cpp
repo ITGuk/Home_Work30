2764c2f9edc629a4b5fa7dde3a73f039e1073540
Pasteras <128259268+Pasteras@users.noreply.github.com>
2023-06-09 17:59:41 +0300

    Add files via upload

---
+++ Task2/H_W30Task2.cpp
@@ -0,0 +1,43 @@
+﻿//Task2
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	int size = 11;
+	char* str = new char[size];
+
+	cout << "\nEnter string: ";	
+	cin >> str;
+
+	bool boo1 = false;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (str[i + 1] != '\0')
+		{
+			if (str[i] <= str[i + 1])
+			{
+				boo1 = true;
+			}
+			else
+			{
+				boo1 = false;
+				break;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (boo1)
+	{
+		cout << "\nYES\n";
+	}
+	else
+	{
+		cout << "\nNO\n";
+	}
+}
